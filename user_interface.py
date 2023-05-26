985b50e101e580ab9e8491f5a46012b9368eed10
cgrundman <92883095+cgrundman@users.noreply.github.com>
2023-05-26 15:11:10 +0200

    updates user interface, adds rpi icon

--- user_interface.py
+++ user_interface.py
@@ -7,7 +7,7 @@ customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dar
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
 app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-app.geometry("800x400")
+app.geometry("1200x600")
 
 
 def button_click():
@@ -23,8 +23,14 @@ def close():
 
 
 # Load the PNG image
-image = Image.open("static/images/hugging-face.png")
-photo = ImageTk.PhotoImage(image)
+height = 200
+width = 200
+hf_image = Image.open("static/images/hugging-face.png")
+hf_image = hf_image.resize((width, height))
+hf_photo = ImageTk.PhotoImage(hf_image)
+rp_image = Image.open("static/images/rasp-pi.png")
+rp_image = rp_image.resize((width, height))
+rp_photo = ImageTk.PhotoImage(rp_image)
 
 # Create three rounded buttons
 button2 = ttk.Button(app, text="Button 2", command=button2_click, style="RoundedButton.TButton")
@@ -34,11 +40,13 @@ button1 = customtkinter.CTkButton(master=app, text="Button 1", command=button_cl
 close_button = customtkinter.CTkButton(master=app, text="Close", fg_color="red", command=close)
 
 # Create a label to display the image
-label = tk.Label(app, image=photo, background="#242424")
-label.place(relx=0.25, rely=0.5, anchor=tk.CENTER)
+hf_label = tk.Label(app, image=hf_photo, background="#242424")
+hf_label.place(relx=0.25, rely=0.5, anchor=tk.CENTER)
+rp_label = tk.Label(app, image=rp_photo, background="#242424")
+rp_label.place(relx=0.75, rely=0.5, anchor=tk.CENTER)
 
-button1.place(relx=0.75, rely=0.5, anchor=tk.CENTER)
-close_button.place(relx=0.75, rely=0.75, anchor=tk.CENTER)
+button1.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+close_button.place(relx=0.5, rely=0.75, anchor=tk.CENTER)
 
 # Start the Tkinter event loop
 app.mainloop()
