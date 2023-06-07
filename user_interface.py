df2b27f96c3b763e633308b85fc97afce4628756
cgrundman <92883095+cgrundman@users.noreply.github.com>
2023-06-07 22:44:39 +0200

    updates user interface libraries in requirements.txt

--- user_interface.py
+++ user_interface.py
@@ -6,7 +6,7 @@ import customtkinter
 import os
 import random
 import PyPDF2
-import docx
+# import docx
 
 customtkinter.set_appearance_mode("light")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
