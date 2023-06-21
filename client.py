a936a4323da2ba288549b49575f7a1c799bdc69b
Khanh Ho <hpk0304@gmail.com>
2023-06-21 12:16:09 +0200

    Update new filters and score ranking

---
+++ client.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+def query_and_date(query, start_date, end_date):
+    # Set the URL of the server
+    # If running on raspberry pi replace <RASPBERRY_PI_IP_ADDRESS> with the actual IP address
+    #server_url = 'http://raspberrypi:5000/predict'
+    server_url = 'http://0.0.0.1:5000/predict'
+
+    data = {'query': query, 'since': start_date, 'until': end_date}
+
+    # Send the request to the server
+    response = requests.post(server_url, json=data)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        response = response.json()
+
+        # Sort and rank tweets
+        sorted_tweets = sorted(response, key=lambda x: x['prediction'])
+        ranked_tweets = [{'rank': rank+1, **tweet} for rank, tweet in enumerate(sorted_tweets)]
+
+        top3 = ranked_tweets[-3:]
+        low3 = ranked_tweets[:3]
+
+        # Their accordingly contents
+        top3_contents = [tweet['content'] for tweet in top3]
+        low3_contents = [tweet['content'] for tweet in low3]
+
+        for tweet in response:
+            print(f"User: {tweet['username']}\tDate: {tweet['date']}")
+            print(tweet['content'])
+            print(f"\nSentiment Score: {tweet['prediction']}")
+            print("\n- - - - - - - - - - - - -\n")
+
+        return top3, low3, top3_contents, low3_contents
+    
+    else:
+        print('Error:', response.status_code)
+        return None, None, None, None
+
+
--- user_interface.py
+++ user_interface.py
@@ -1,66 +1,158 @@
 import tkinter as tk
-from tkinter import ttk, filedialog
-from tkinter.filedialog import askopenfile
+from tkinter import ttk
 from PIL import ImageTk, Image
 import customtkinter
-import os
-import random
-import PyPDF2
-import docx
+from tkcalendar import Calendar
+import requests
+from client import query_and_date
+import re
 
-customtkinter.set_appearance_mode("light")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+customtkinter.set_appearance_mode("light")
+customtkinter.set_default_color_theme("blue")
 
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-app.geometry("1200x1000")
+app = customtkinter.CTk()
+app.geometry("1200x1200")
 app.configure(bg='#dceaea')
 app.configure(highlightthickness=0)
-
-def browse_click():
-    # The buttion changes to 'Loading...' when clicking the button
-    browse_button.configure(text='Loading...')
-    app.update()
-    
-    # Give the path, filedialog from tkinter
-    file = askopenfile(mode='rb', title='Choose a file', 
-                       filetype=[('pdf file', '*.pdf')]) 
-    
-    if file:
-        # Read the file
-        pdf = PyPDF2.PdfReader(file)
-        page = pdf.pages[0]
-        content = page.extract_text()
-
-        # If finishing upload, change the name of browser_button
-        browse_button.configure(text='Done', fg_color='#538888')
-
-        # Display the file name
-        file_label.configure(text=file.name) 
-
-    else:
-        # No file is selected, revert the Browser
-        browse_button.configure(text='Browse')
-
-    print('Browse clicked')
-
-
-def analysis_click():
-    print("Analysis clicked!")
-
-
-def button2_click():
-    print("Button 2 clicked!")
-
-
-def close():
-    app.destroy()
 
 # get the height of app
 app.update()
 org_height = app.winfo_height()
 org_width = app.winfo_width()
 
-# Load the PNG image
+start_cal = None
+end_cal = None
+
+def select_time():
+    global start_cal, end_cal, start_date, end_date
+
+    def select_date():
+        start_date = start_cal.get_date()
+        end_date = end_cal.get_date()
+        selected_date.configure(text='Since {} Until {}'.format(start_date, end_date))
+        start_cal.destroy() # Remove calendar after time is selected
+        end_cal.destroy()
+        select_button.destroy()
+
+    start_cal = Calendar(app, selectmode='day', date_pattern='yyyy-mm-dd')
+    start_cal.place(relx=0.265, rely=0.55, anchor=tk.W)
+    end_cal = Calendar(app, selectmode='day', date_pattern='yyyy-mm-dd')
+    end_cal.place(relx=0.395, rely=0.55, anchor=tk.W)
+
+    select_button = ttk.Button(app, text='Select', command=select_date)
+    select_button.place(relx=0.265, rely=0.65, anchor=tk.W)
+
+def analysis_click():
+    # Retrieve the query from the input box
+    query = input.get('1.0', tk.END).strip()
+
+    # Reset the analysis button text to "Analysis"
+    analysis_button.configure(text='Analysis', command=analysis_click)
+
+    # Print the query and perform analysis
+    print(f"Query: {query}")
+    analysis_retrieve(query)
+    print("Analysis Done!")
+
+def analysis_retrieve(query):
+    # Retrieve the query from the input box
+    query = input.get('1.0', tk.END).strip()
+
+    # Retrieve the start and end dates from the global variables
+    start_date = start_cal.get_date()
+    end_date = end_cal.get_date()
+
+    # Call the query_and_date function with the query, start_date, and end_date
+    query_and_date(query, start_date, end_date)
+    top3, low3, top3_contents, low3_contents = query_and_date(query, start_date, end_date)
+
+    # Create Table widget (Treeview):
+    global top3_tree, low3_tree
+    top3_tree = ttk.Treeview(app, columns=('Score', 'Content'), show='headings')
+    low3_tree = ttk.Treeview(app, columns=('Score', 'Content'), show='headings')
+
+    # Set style and font for score and content
+    style = ttk.Style()
+    style.configure('Treeview', font=('Helvetica', 12))
+    style.configure('Treeview.Heading', font=('Helvetica', 12, 'bold'))
+    
+    # Headings
+    top3_tree.heading('Score', text='Highest Score')
+    top3_tree.heading('Content', text='Content')
+    low3_tree.heading('Score', text='Lowest Score')
+    low3_tree.heading('Content', text='Content')
+
+    # Widths
+    top3_tree.column('Score', width=30)
+    top3_tree.column('Content', width=500)
+    low3_tree.column('Score', width=30)
+    low3_tree.column('Content', width=500)
+
+    # Location
+    top3_tree.place(relx=0.05, rely=0.62, relwidth=0.4, relheight=0.2)
+    low3_tree.place(relx=0.55, rely=0.62, relwidth=0.4, relheight=0.2)
+
+    # Clear existing tables:
+    top3_tree.delete(*top3_tree.get_children())
+    low3_tree.delete(*top3_tree.get_children())
+
+    # Display 5 highest and 5 lowest
+    for i, score in enumerate(top3[:3]):
+        score_value = round(float(score['prediction']),3)
+
+        # Constraint the length of the content and continue in newline if it exceeds the length
+        content = top3_contents[i]
+        content = remove_info(content)  # Remove links and mentions
+        if len(content) > 100:
+            content = '\n'.join([content[i:i+100] for i in range(0, len(content), 100)])
+        # Insert contents
+        top3_tree.insert('', 'end', values=(f'{score_value:.3f}', content))
+
+
+    for i, score in enumerate(low3[:3]):
+        score_value = round(float(score['prediction']),3)        
+        content = low3_contents[i]
+        content = remove_info(content)  # Remove links and mentions
+        if len(content) > 100:
+            content = '\n'.join([content[i:i+100] for i in range(0, len(content), 100)])
+        
+        low3_tree.insert('', 'end', values=(f'{score_value:.3f}', content))
+
+    # Create scrollbars for the Content column
+    top3_content_scrollbar = ttk.Scrollbar(app, orient='horizontal', command=top3_tree.xview)
+    low3_content_scrollbar = ttk.Scrollbar(app, orient='horizontal', command=low3_tree.xview)
+
+    # Configure scrollbars
+    top3_tree.configure(xscrollcommand=top3_content_scrollbar.set)
+    low3_tree.configure(xscrollcommand=low3_content_scrollbar.set)
+
+    # Set the scrollbar width and height
+    top3_content_scrollbar.place(in_=top3_tree, relx=0, rely=1, relwidth=1, anchor=tk.SW)
+    low3_content_scrollbar.place(in_=low3_tree, relx=0, rely=1, relwidth=1, anchor=tk.SW) 
+
+def remove_info(content):
+    # Remove links
+    content = re.sub(r'http\S+|www\S+', '', content)
+    # Remove mentions
+    content = re.sub(r'@\w+', '', content)
+    return content
+
+def clear_table(table):
+    table.delete('1.0', tk.END)
+
+def insert_row(table, score, content):
+    table.insert(tk.END, f"Score: {score:.3f}\n")
+    table.insert(tk.END, f"Content: {content}\n\n")
+    num_lines = content.count('\n') + 1
+    table.configure(height=num_lines)
+
+def button2_click():
+    print("Button 2 clicked!")
+
+def close():
+    app.destroy()
+
+# Create Images:
 height = 200
 width = 200
 hf_image = Image.open("static/images/hugging-face.png")
@@ -72,22 +164,31 @@ rp_photo = ImageTk.PhotoImage(rp_image)
 bg_image = Image.open("static/images/emoji1.png")
 bg_photo = ImageTk.PhotoImage(bg_image)
 
-# Create three rounded buttons
-button2 = ttk.Button(app, text="Button 2", command=button2_click, style="RoundedButton.TButton")
+hf_label = tk.Label(app, image=hf_photo, background="#EBEBEB")
+hf_label.place(relx=0.1, rely=0.5, anchor=tk.CENTER)
+rp_label = tk.Label(app, image=rp_photo, background="#EBEBEB")
+rp_label.place(relx=0.9, rely=0.5, anchor=tk.CENTER)
 
-# Add buttons to the window with spacing
-browse_button = customtkinter.CTkButton(app, text='Browse', fg_color='#264040', command=browse_click)
-analysis_button = customtkinter.CTkButton(master=app, text="Analysis", fg_color='#de9304', command=analysis_click)
-close_button = customtkinter.CTkButton(master=app, text="Close", fg_color="#b01003", command=close)
-
-# Create a box on the left corner
+# Create Frame on top (under SENTIMENT Text)
 frame = tk.Frame(app, width=org_width, height=344, bg='#ABCDCD')
-frame.pack_propagate(0) # not resize the frame to fix the content
+frame.pack_propagate(0)
 frame.pack(side='top', anchor=tk.CENTER)
+bg_label = tk.Label(frame, image=bg_photo, background='#ABCDCD')
+bg_label.place(relx=1, rely=0, anchor=tk.NE)
 
-# Create a label to display texts
-header_label = tk.Label(app, text = 'SENTIMENT ANALYSIS', font=('Helvetica', 48, 'bold'), 
-                        fg='#1f3333', bg='#abcdcd', bd=0) # insert text on the box
+# Create Buttons
+button2 = ttk.Button(app, text="Button 2", command=button2_click, style="RoundedButton.TButton")
+calendar_button = customtkinter.CTkButton(app, text='Open Calendar', fg_color='#264040', command=select_time)
+analysis_button = customtkinter.CTkButton(master=app, text='Analysis', fg_color='#de9304', command=analysis_click)
+close_button = customtkinter.CTkButton(master=app, text='Close', fg_color="#b01003", command=close)
+
+calendar_button.place(relx=0.265, rely=0.55, anchor=tk.W)
+analysis_button.place(relx=0.68, rely=0.55, anchor=tk.CENTER)
+close_button.place(relx=0.9, rely=0.9, anchor=tk.CENTER)
+
+# Create text:
+header_label = tk.Label(app, text='SENTIMENT ANALYSIS', font=('Helvetica', 48, 'bold'), 
+                        fg='#1f3333', bg='#abcdcd', bd=0)
 header_label.place(relx=0.3, rely=0.1, anchor=tk.CENTER)
 
 introduction = """
@@ -96,79 +197,22 @@ dive deep into emotions, embrace contextual understanding,
 customize analysis, visualize with flair, and trust in data privacy.
 """
 introduction_label = tk.Label(app, text=introduction, font=('Helvetica', 18),
-                              fg='#1f3333', bg='#abcdcd', bd=0, highlightthickness=0) # introduction, set borderwidth bd=0 
+                              fg='#1f3333', bg='#abcdcd', bd=0, highlightthickness=0)
 introduction_label.place(relx=0.3, rely=0.21, anchor=tk.CENTER)
 
-input_text = tk.Label(app, text='Please type your ideas or upload a file:', font=('Helvetica', 15),
-                     fg='#1f3333', bg='#ebebeb') # instruct using text box or browse
+input_text = tk.Label(app, text='What are you searching for?', font=('Helvetica', 15),
+                     fg='#1f3333', bg='#ebebeb')
 input_text.place(relx=0.265, rely=0.42, anchor=tk.W)
 
-instruction = tk.Label(app, text='File format: .pdf', font=('Helvetica', 15),
+selected_date = tk.Label(app, text='Date Range: ', font=('Helvetica', 15),
                        fg='#1f3333', bg='#ebebeb')
-instruction.place(relx=0.39, rely=0.59, anchor=tk.W)
+selected_date.place(relx=0.38, rely=0.55, anchor=tk.W)
 
 file_label = tk.Label(app, text='', font=('Helvetica', 15),
-                      fg='white', bg='#242424') # show the file name
+                      fg='white', bg='#242424')
 file_label.place(relx=0.38, rely=0.49, anchor=tk.W)
 
-# Create text widgets
-input = tk.Text(app, width=60, height=3, font=('Helvetica', 20)) # entry the text info
-input.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+input = tk.Text(app, width=60, height=2, font=('Helvetica', 20))
+input.place(relx=0.5, rely=0.48, anchor=tk.CENTER)
 
-
-# Create a label to display the image
-hf_label = tk.Label(app, image=hf_photo, background="#EBEBEB")
-hf_label.place(relx=0.1, rely=0.5, anchor=tk.CENTER)
-rp_label = tk.Label(app, image=rp_photo, background="#EBEBEB")
-rp_label.place(relx=0.9, rely=0.5, anchor=tk.CENTER)
-bg_label = tk.Label(frame, image=bg_photo, background='#ABCDCD')
-bg_label.place(relx=1, rely=0, anchor=tk.NE)
-
-browse_button.place(relx=0.265, rely=0.59, anchor=tk.W)
-analysis_button.place(relx=0.68, rely=0.59, anchor=tk.CENTER)
-close_button.place(relx=0.9, rely=0.9, anchor=tk.CENTER)
-
-# Create sentimental states - progress bars for each state
-states = ['happy', 'positive', 'neutral','negative','angry']
-colors = {'happy':'#12831f', 'positive':'#1e8aec','neutral':'#ffad1e',
-          'negative': '#d76213','angry': '#c31c2e'}
-
-labels = {}
-progress_bar = {}
-y = 0.65 # coordinates to move the bars and states
-offset = 0.05 # coordinates to move the bars and states
-
-for state in states:
-
-    label = tk.Label(app, text=f'{state}: 0%', fg=colors[state], font=('Helvetica', 20, 'bold'),
-                     bg='#EBEBEB')
-    label.place(relx=0.55, rely=y)
-
-    canvas = tk.Canvas(app, width= 300, height= 30) # create canvas to fill in the colors
-    canvas.place(relx=0.35, rely=y)
-
-    progress = canvas.create_rectangle(0,0,0,30,fill=colors[state])
-
-    labels[state] = label
-    progress_bar[state] = (canvas,progress)
-
-    y += offset
-
-
-def update_bars():
-    for state in states:
-        new_value = random.randint(0,100) # stimulate values
-
-        canvas, progress = progress_bar[state]
-        canvas.coords(progress, (0,0,3*new_value,30)) # width=2*percentages
-
-        labels[state]['text'] = f'{state}: {new_value}%' # update label
-    
-    app.after(500, update_bars) # update every 500ms
-
-# Start updates
-update_bars()
-
-
-# Start the Tkinter event loop
 app.mainloop()
